907a70b84b8f3783508585c05472eb7a022a1d2a
Javari <javari@duck.com>
2022-12-10 01:41:12 +0330

    Add cmd class

--- libs/utils.py
+++ libs/utils.py
@@ -3,3 +3,11 @@ import time
 import json
 import os
 
+# cmd
+class cmd:
+    ## clear screen
+    def clear():
+        os.system('clear' if os.name == 'posix' else 'cls')
+    ## run command
+    def run(path):
+        os.system(path)
