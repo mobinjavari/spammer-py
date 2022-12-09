c8332c8ea841e4db207591f2da871c8916f491e1
Javari <javari@duck.com>
2022-12-10 01:49:58 +0330

    Calling methods in classes

--- console.py
+++ console.py
@@ -2,3 +2,13 @@ from libs.utils import spam
 from libs.utils import cmd
 from libs.utils import text
 
+try:
+
+    while True:
+        cmd.clear()
+        text.welcome()
+        spam.menu()
+
+except:
+    print(text.status("*","You Exit Tools !!\n"))
+
